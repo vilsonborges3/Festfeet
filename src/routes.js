d0e2870ff42565fbad864c63c6b7fb7d5b541897
Vilson Borges <vilsonborges98@gmail.com>
2020-06-17 10:12:03 -0300

    chore: Aplicação pronta para reber primeiras rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '12345678',
+  });
+  return res.json(user);
 });
 
-module.exports = routes;
+export default routes;
