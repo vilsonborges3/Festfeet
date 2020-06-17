ea0f889106db0d389ddcd8a33840f165696e3551
Vilson Borges <vilsonborges98@gmail.com>
2020-06-17 12:24:22 -0300

    chore: autenticação de usuarios

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8e137ae2e636102d4090f8f72636c9a0',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '12345678',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
