0219257a314828ea99e979d6e0cafb68fede159a
Vilson Borges <vilsonborges98@gmail.com>
2020-06-22 09:41:11 -0300

    upload de arquivo para avatar de entregadores

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
 import DeliversCotrolers from './app/controllers/DeliversControllers';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-routes.post(
-  '/createRecipients',
-  authMiddleware,
-  UserController.createRecipients
-);
 
-routes.put(
-  '/updateRecipients',
-  authMiddleware,
-  UserController.updateRecipients
-);
+routes.use(authMiddleware);
+routes.post('/createRecipients', UserController.createRecipients);
 
-routes.post(
-  '/createDelivers',
-  authMiddleware,
-  DeliversCotrolers.createDelivers
-);
+routes.put('/updateRecipients', UserController.updateRecipients);
+
+routes.post('/createDelivers', DeliversCotrolers.createDelivers);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json('ok');
+});
 
 export default routes;
